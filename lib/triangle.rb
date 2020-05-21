3d96d517121fd7b04f68c146431855573d6a917e
“dinadeljanin” <ddeljanin5@icloud.com>
2020-05-21 19:39:34 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+
+  attr_accessor :first, :second, :third
+
+  def initialize(attributes)
+    attributes.each {|key, val| self.send(("#{key}="), val)}
+  end
+
+  def check
+    arr = [first, second, third].sort
+    if arr.any?(&:negative?) || arr.include?(0)
+      raise TriangleError
+    elsif arr[0] + arr[1] <= arr[2]
+      raise TriangleError
+    else
+      return arr
+    end
+  end
+
+
+  def kind
+    case check.uniq.count
+    when 1
+      :equilateral
+    when 2
+      :isosceles
+    when 3
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    puts "Illegal :("
+  end
 end
